b328ac1fe0ac4e557e52f7fd373b1da2c8506843
agus@mumuki.org <agus@mumuki.org>
2020-07-17 15:31:37 +0000

    Mumuki Export on 2020-07-17 15:31:37 +0000

--- 00007_Contiene/test..js
+++ 00007_Contiene/test..js
@@ -1,18 +1,18 @@
 describe('', () => {
     it('contiene([1, 6, 7, 6], 7) es true', () => {
         assert(contiene([
-            '1',
-            '6',
-            '7',
-            '6'
+            1,
+            6,
+            7,
+            6
         ], 7));
     });
     it('contiene([1, 6, 7, 6], 6) es true', () => {
         assert(contiene([
-            '1',
-            '6',
-            '7',
-            '6'
+            1,
+            6,
+            7,
+            6
         ], 6));
     });
     it('contiene([], 7) es false', () => {
@@ -20,22 +20,22 @@ describe('', () => {
     });
     it('contiene([8, 5], 7) es false', () => {
         assert(!contiene([
-            '8',
-            '5'
+            8,
+            5
         ], 7));
     });
     it('contiene([1, 8, 5], 7) es false', () => {
         assert(!contiene([
-            '1',
-            '8',
-            '5'
+            1,
+            8,
+            5
         ], 7));
     });
     it('contiene([1, 1, 1], 1) es true', () => {
         assert(contiene([
-            '1',
-            '1',
-            '1'
+            1,
+            1,
+            1
         ], 1));
     });
 });
